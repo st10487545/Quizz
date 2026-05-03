13f1deaf5d61283c281a1840e0553bbba555212d
st10487545 <ST10487545@VCCONNECT.EDU.ZA>
2026-05-03 10:50:41 +0200

    commit

---
+++ src/main/java/com/mycompany/quizz/Quizz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quizz;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Quizz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
